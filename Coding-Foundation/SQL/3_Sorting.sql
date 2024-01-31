dbf443317934757d33a72c6163a0c05c66da5aaa
rakeshyadav-19 <rakeshyadavry087@gmail.com>
2024-01-31 17:08:26 +0530

    chap 3 Completed

--- Coding-Foundation/SQL/3_Sorting.sql
+++ Coding-Foundation/SQL/3_Sorting.sql
@@ -5,3 +5,6 @@ ORDER BY fields
 
 -- Your extracted data will be sorted in ascending order by default. If you need the data sorted in descending order (from largest to smallest) you need to add the DESC keyword. 
 ORDER BY field DESC
+
+--We can also sort data that constais text, it will by default sort in ascending order thatis Alphabetical order
+ORDER BY field ASC
