ec1fede17ba6c6a71861108d79e3d28bdb4ddf79
rakesyadav19 <rakeshyadavry087@gmail.com>
2024-02-01 11:12:58 +0530

    lets start

---
+++ CodeWithHarry/JavaScript/Html,Css,Js/28_window_BOM_DOM.js
@@ -0,0 +1,11 @@
+//It i global object
+//Everything comes under window object
+//E.G window.alert("Message")
+console.log(window)
+//OR window.console.log(window)
+console.log(document.body)
+//we Change the html body depending on user action for eg
+document.body.style.background = "red"
+
+//To send user to some location on ation
+//location.href = "https:// dsf";
