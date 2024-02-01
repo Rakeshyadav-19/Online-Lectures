3f39ebd5174c39f675650ddb43e4f25999062514
Rakeshyadav-19 <62337250+Rakeshyadav-19@users.noreply.github.com>
2024-02-01 15:16:20 +0000

    soll

--- CodeWithHarry/JavaScript/Html,Css,Js/35_TabelBasedNavigation.js
+++ CodeWithHarry/JavaScript/Html,Css,Js/35_TabelBasedNavigation.js
@@ -1,17 +1,21 @@
-let t = document.body.firstElementChild.firstElementChild
-console.log(t)
+let t = document.body.firstElementChild.firstElementChild;
+console.log(t);
 //colection of tr elements
-console.log(t.rows)
+console.log(t.rows);
 //reference to caption
-console.log(t.caption)
+console.log(t.caption);
 //reference to thead
-console.log(t.tHead.firstElementChild)
+console.log(t.tHead.firstElementChild);
 //reference to tfoot
-console.log(t.tFoot)
+console.log(t.tFoot);
 //reference to tbody
-console.log(t.tBodies)
-//
-console.log(tbody.rows)
-
-
-console.log(t.rows[1].rowIndex)
+console.log(t.tBodies);
+//collection of tr inside element
+console.log(t.rows[0].rowIndex);
+//collection of td and th
+console.log(t.rows[0].cells);
+//collection of td and th
+console.log(t.rows[0].sectionRowIndex);
+//collection of tr inside element
+console.log(t.rows[0].rowIndex);
+// typeof document and typeof window is object
