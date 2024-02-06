b69735f3f0afdeacae185ff0c5d4b92a25d57144
rakesyadav19 <rakeshyadavry087@gmail.com>
2024-02-06 14:03:58 +0530

    clock

--- CodeWithHarry/JavaScript/Html,Css,Js/51_Exercise4.js
+++ CodeWithHarry/JavaScript/Html,Css,Js/51_Exercise4.js
@@ -1,24 +1,38 @@
 //Q Create a Digital Seconds clock using setInterval method in JS, make UI.
 //Use date object to get date
-let h = 1
-let m = 1
-let s = 0
 
 setInterval(function(){
-    document.getElementById("Hr").innerHTML = h //`${h}`
-    h++
-}, 360000)
-setInterval(function(){
-    document.getElementById("Min").innerHTML = m //`${m}`
-    m++
-}, 60000)
-setInterval(function(){
-    document.getElementById("Sec").innerHTML = s //`${s}`
-    s++
-}, 1000)
+    let a = new Date()
+    let h = a.getHours()
+    document.getElementById("Hr").innerHTML =  `${h}:`
+    let m = a.getMinutes()
+    document.getElementById("Min").innerHTML = `${m}:`
+    let s = a.getSeconds()
+    document.getElementById("Sec").innerHTML = `${s}`
+    
+    let wek = a.toLocaleDateString("india", { weekday: 'short' })
+    document.getElementById("week").innerHTML = `<pre>${wek} `
+    let mon = a.toLocaleString("default", { month: "short" })
+    document.getElementById("month").innerHTML = `<pre>${mon} `
+    let day = a.getDate()
+    document.getElementById("day").innerHTML = `<pre>${day}`
+    //console.log(a,h,m,s,d)
+},1000)
 
-// let a = new Date()
-// let h = a.getHours()
-// let m = a.getMinutes()
-// let s = a.getSeconds()
-// let d = a.getDate()
+// let h = 1
+// let m = 1
+// let s = 0
+// setInterval(function(){
+//     document.getElementById("Hr").innerHTML = h //`${h}`
+//     h++
+// }, 360000)
+// setInterval(function(){
+//     document.getElementById("Min").innerHTML = m //`${m}`
+//     m++
+// }, 60000)
+// setInterval(function(){
+//     document.getElementById("Sec").innerHTML = s //`${s}`
+//     s++
+// }, 1000)
+
+
