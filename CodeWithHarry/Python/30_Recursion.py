1e0708432f4c99821818a5519c733064c57d6ffb
Rakesh <62337250+Rakeshyadav-19@users.noreply.github.com>
2024-02-26 19:54:49 +0530

    Recursion done

--- CodeWithHarry/Python/30_Recursion.py
+++ CodeWithHarry/Python/30_Recursion.py
@@ -6,3 +6,16 @@ def factorial(n):
   else:
     return n * factorial(n - 1)
 print(factorial(5))
+
+# For fibonaaci series
+#f(n) = f(n-1) + f(n-2)
+def fibo(n):
+    f1 =0
+    f2 = 1
+    for i in range (1,n+1):
+      fn = f1+f2
+      f1 =f2
+      f2 =fn
+    return f2
+  
+print(fibo(10))
