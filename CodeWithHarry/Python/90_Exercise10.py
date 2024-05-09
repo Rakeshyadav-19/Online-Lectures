3e5735a2a320ab17ad6e953fc3bc89ea9f3e5727
rakesyadav19 <rakeshyadavry087@gmail.com>
2024-05-09 19:38:05 +0530

    read it

--- CodeWithHarry/Python/90_Exercise10.py
+++ CodeWithHarry/Python/90_Exercise10.py
@@ -1,2 +1,23 @@
 # Use the NewsAPI and the requests module to fetch the daily news related to different topics. 
 # Go to: https://newsapi.org/ and explore the various options to build you application.
+
+# 8bbbd9d569b5486a809ddf3598d97b9a
+
+import requests
+import json 
+
+url = ('https://newsapi.org/v2/everything?'
+       'country=IN&'
+       'sources=bbc-news&'
+       'apiKey=8bbbd9d569b5486a809ddf3598d97b9a')
+
+# url = ('https://newsapi.org/v2/everything?'
+#        'q=Apple&'
+#        'from=2024-05-07&'
+#        'sortBy=popularity&'
+#        'apiKey=8bbbd9d569b5486a809ddf3598d97b9a')
+
+
+response = requests.get(url)
+result = response.json()
+print(json.dumps(result, indent=4, sort_keys=True))
